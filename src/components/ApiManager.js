53414749faa38ac349b6ae84e91c6f2f5123e982
Lance Buckley <erchancy@gmail.com>
2023-06-08 14:47:21 -0500

    Added Edit and Host Game Buttons, where Host Game on click creates a session object

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -9,8 +9,25 @@ export const createSettlement = (newSettlement) => {
         .then(response => response.json())
 }
 
+export const createSession = (session) => {
+    return fetch("http://localhost:8088/sessions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(session)
+    })
+        .then(response => response.json())
+}
+
 export const getSettlements = async () => {
     const response = await fetch("http://localhost:8088/settlements")
     const settlements = await response.json()
     return settlements
+}
+
+export const getSessions = async () => {
+    const response = await fetch("http://localhost:8088/sessions")
+    const sessions = await response.json()
+    return sessions
 }
--- src/components/home/GMHomeScreen.js
+++ src/components/home/GMHomeScreen.js
@@ -1,8 +1,19 @@
 import { useEffect, useState } from "react"
-import { getSettlements } from "../ApiManager"
+import { createSession, getSessions, getSettlements } from "../ApiManager"
 
 export const GMHomeScreen = () => {
     const [settlements, setSettlements] = useState([])
+    const [sessions, setSessions] = useState([])
+    const [filteredSettlements, setFilteredSettlements] = useState([])
+    const [filteredSessions, setFilteredSessions] = useState([])
+    const [session, setSession] = useState({
+        gameMasterId: 0,
+        settlementId: 0
+    })
+
+    const localDeathUser = localStorage.getItem("kdm_user")
+    const deathUserObject = JSON.parse(localDeathUser)
+
 
     useEffect(
         () => {
@@ -14,22 +25,69 @@ export const GMHomeScreen = () => {
         []
     )
 
+    useEffect(
+        () => {
+            getSessions()
+                .then((sessions) => {
+                    setSessions(sessions)
+                })
+        },
+        []
+    )
+
+    useEffect(
+        () => {
+            const mySettlements = settlements.filter(settlement => settlement.userId === deathUserObject.id)
+            setFilteredSettlements(mySettlements)
+        },
+        [settlements]
+    )
+
+    useEffect(
+        () => {
+            const mySessions = sessions.filter(session => session.gameMasterId === deathUserObject.id)
+            setFilteredSessions(mySessions)
+        },
+        [sessions]
+    )
+
+    useEffect(
+        () => {
+            if (session.settlementId !== 0) {
+                createSession(session)
+            }
+        },
+        [session]
+    )
+
     const editButton = () => {
         return <button>Edit</button>
     }
 
-    const hostGameButton = () => {
-        return <button>Host Game?</button>
+    const hostGameButton = (settlementId) => {
+        const findSession = filteredSessions.find(filteredSession => { return filteredSession.settlementId === settlementId })
+        if (findSession) {
+            return <>Hosting!</>
+        } else {
+            return <button onClick={() => { hostSession(settlementId) }}>Host Game?</button>
+        }
+    }
+
+    const hostSession = (settlementId) => {
+        const copy = { ...session }
+        copy.settlementId = settlementId
+        copy.gameMasterId = deathUserObject.id
+        setSession(copy)
     }
 
     return <>
         <ul className="settlement__list">My Settlements
             {
-                settlements.map((settlement) => (
+                filteredSettlements.map((settlement) => (
                     <li className="settlement__item" key={`settlement-${settlement.id}`}>
                         {settlement.name}
                         {editButton()}
-                        {hostGameButton()}
+                        {hostGameButton(settlement.id)}
                     </li>
                 ))
 
