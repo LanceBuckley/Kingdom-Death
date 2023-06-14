45e0af5be1920342f80c4260a13595d0387fbfae
Lance Buckley <erchancy@gmail.com>
2023-06-14 16:08:06 -0500

    Added SaveSurvivor

---
+++ src/components/survivors/SaveSurvivor.js
@@ -0,0 +1,13 @@
+export const SaveSurvivor = () => {
+
+    const handleSaveButtonClick = () => {
+        return <></>
+    }
+    
+    return <button
+    onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+    className="btn btn-primary">
+    Submit Survivor
+    </button>
+    }
+    
