f3ff4204b45216a1be5d32701de7c52fa0a08cad
Lance Buckley <erchancy@gmail.com>
2023-06-19 16:02:02 -0500

    More css, bleh

--- src/components/home/GMHomeScreen.js
+++ src/components/home/GMHomeScreen.js
@@ -1,106 +1,112 @@
-import { useEffect, useState } from "react"
-import { createSession, getSessions, getSettlements, getSurvivors } from "../ApiManager"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { createSession, getSessions, getSettlements } from "../ApiManager";
+import { Link } from "react-router-dom";
+import "./GMHome.css";
+
 
 export const GMHomeScreen = () => {
-    const [settlements, setSettlements] = useState([])
-    const [sessions, setSessions] = useState([])
-    const [filteredSettlements, setFilteredSettlements] = useState([])
-    const [filteredSessions, setFilteredSessions] = useState([])
+    const [settlements, setSettlements] = useState([]);
+    const [sessions, setSessions] = useState([]);
+    const [filteredSettlements, setFilteredSettlements] = useState([]);
+    const [filteredSessions, setFilteredSessions] = useState([]);
     const [session, setSession] = useState({
         gameMasterId: 0,
         settlementId: 0
-    })
+    });
 
-    const localDeathUser = localStorage.getItem("kdm_user")
-    const deathUserObject = JSON.parse(localDeathUser)
+    const localDeathUser = localStorage.getItem("kdm_user");
+    const deathUserObject = JSON.parse(localDeathUser);
 
+    useEffect(() => {
+        getSettlements().then((settlements) => {
+            setSettlements(settlements);
+        });
+    }, []);
 
-    useEffect(
-        () => {
-            getSettlements()
-                .then((settlements) => {
-                    setSettlements(settlements)
-                })
-        },
-        []
-    )
+    useEffect(() => {
+        getSessions().then((sessions) => {
+            setSessions(sessions);
+        });
+    }, []);
 
-    useEffect(
-        () => {
-            getSessions()
-                .then((sessions) => {
-                    setSessions(sessions)
-                })
-        },
-        []
-    )
+    useEffect(() => {
+        const mySettlements = settlements.filter(
+            (settlement) => settlement.userId === deathUserObject.id
+        );
+        setFilteredSettlements(mySettlements);
+    }, [settlements]);
 
-    useEffect(
-        () => {
-            const mySettlements = settlements.filter(settlement => settlement.userId === deathUserObject.id)
-            setFilteredSettlements(mySettlements)
-        },
-        [settlements]
-    )
+    useEffect(() => {
+        const mySessions = sessions.filter(
+            (session) => session.gameMasterId === deathUserObject.id
+        );
+        setFilteredSessions(mySessions);
+    }, [sessions]);
 
-    useEffect(
-        () => {
-            const mySessions = sessions.filter(session => session.gameMasterId === deathUserObject.id)
-            setFilteredSessions(mySessions)
-        },
-        [sessions]
-    )
-
-    useEffect(
-        () => {
-            if (session.settlementId !== 0) {
-                createSession(session)
+    useEffect(() => {
+        if (session.settlementId !== 0) {
+            createSession(session)
                 .then(getSettlements)
                 .then((settlements) => {
-                    setSettlements(settlements)
+                    setSettlements(settlements);
                 })
                 .then(getSessions)
                 .then((sessions) => {
-                    setSessions(sessions)
-                })
-            }
-        },
-        [session]
-    )
+                    setSessions(sessions);
+                });
+        }
+    }, [session]);
 
     const editButton = (settlementId) => {
-        return <Link to={`/settlement/${settlementId}`}><button>Edit</button></Link>
-    }
+        return (
+            <Link to={`/settlement/${settlementId}`}>
+                <button className="button is-dark is-small">Edit</button>
+            </Link>
+        );
+    };
 
     const hostGameButton = (settlementId) => {
-        const findSession = filteredSessions.find(filteredSession => { return filteredSession.settlementId === settlementId })
+        const findSession = filteredSessions.find(
+            (filteredSession) => filteredSession.settlementId === settlementId
+        );
         if (findSession) {
-            return <> Hosting!</>
+            return <>Hosting!</>;
         } else {
-            return <button onClick={() => { hostSession(settlementId) }}>Host Game?</button>
+            return (
+                <button
+                    className="button is-link is-small"
+                    onClick={() => {
+                        hostSession(settlementId);
+                    }}
+                >
+                    Host Game?
+                </button>
+            );
         }
-    }
+    };
 
     const hostSession = (settlementId) => {
-        const copy = { ...session }
-        copy.settlementId = settlementId
-        copy.gameMasterId = deathUserObject.id
-        setSession(copy)
-    }
+        const copy = { ...session };
+        copy.settlementId = settlementId;
+        copy.gameMasterId = deathUserObject.id;
+        setSession(copy);
+    };
 
-    return <>
-        <ul className="settlement__list">My Settlements
-            {
-                filteredSettlements.map((settlement) => (
-                    <li className="settlement__item" key={`settlement-${settlement.id}`}>
-                        {settlement.name}
-                        {editButton(settlement.id)}
-                        {hostGameButton(settlement.id)}
+    return (
+        <div className="box container">
+            <ul className="settlement__list columns">
+                <h1 className="title">My Settlements</h1>
+                {filteredSettlements.map((settlement) => (
+                    <li
+                        className="settlement__item column"
+                        key={`settlement-${settlement.id}`}
+                    >
+                        <h1 className="is-size-4">{settlement.name}</h1>
+                        <h3>{editButton(settlement.id)}</h3>
+                        <h3>{hostGameButton(settlement.id)}</h3>
                     </li>
-                ))
-
-            }
-        </ul>
-    </>
-}
+                ))}
+            </ul>
+        </div>
+    );
+};
--- src/components/nav/GMNav.js
+++ src/components/nav/GMNav.js
@@ -1,36 +1,42 @@
-import { Link, Route, useLocation, useNavigate } from "react-router-dom"
-import "./NavBar.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
 
 export const GMNav = () => {
-    const navigate = useNavigate()
-    const location = useLocation()
-
-    const isSettlementPage = location.pathname === "/settlements" || location.pathname.startsWith("/settlement/")
+    const navigate = useNavigate();
 
     return (
-        <ul className="navbar">
-            {
-                isSettlementPage
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            navigate("/")
-                        }}>Home</Link>
-                    </li>
-                    : <li className="navbar__item active">
-                        <Link className="navbar__link" to="/settlements">New Settlement</Link>
-                    </li>
-            }
-            {
-                localStorage.getItem("kdm_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("kdm_user")
-                            navigate("/", { replace: true })
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
-    )
-}
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                    <Link className="navbar-item" to="/">
+                        Home
+                    </Link>
+            </div>
 
+            <div className="navbar-menu">
+                <div className="navbar-start">
+
+                    <Link className="navbar-link" to="/settlements">
+                        New Settlement
+                    </Link>
+                </div>
+
+                <div className="navbar-end">
+                    {localStorage.getItem("kdm_user") ? (
+                        <li className="navbar-item">
+                            <Link
+                                className="navbar-link"
+                                to="/"
+                                onClick={() => {
+                                    localStorage.removeItem("kdm_user");
+                                    navigate("/", { replace: true });
+                                }}
+                            >
+                                Logout
+                            </Link>
+                        </li>
+                    ) : null}
+                </div>
+            </div>
+        </nav>
+    );
+};
